943026ab11c9094319600090317677a01bf2427e
sofia becerra <sofiabj13@hotmail.com>
2024-06-13 13:03:54 -0600

    fallo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'json'
+require 'open-uri' #leer un enlace
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+html_doc = URI.open(url).read  #abre el enlace y leelo, transformandolo en un texto como string
+movies = JSON.parse(html_doc)["results"] #json.parse pasa el string para hash (el html_doc es un string) otorgandonos un ruby hash
+
+
+movies.each do |movie|
+  Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
