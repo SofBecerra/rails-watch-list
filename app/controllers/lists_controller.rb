be796d17ce5dde46ba4ac8acfe7e13fdb080e98f
sofia becerra <sofiabj13@hotmail.com>
2024-06-15 03:40:16 -0600

    con algunos estilos

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -16,10 +16,11 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
+
     if @list.save
       redirect_to lists_path, notice: 'List was successfully created.'
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
